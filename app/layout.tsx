992daedb04a7f18a30eb25e3a03c32aa89f41ce3
Isaladwy <Isaladwy@gmail.com>
2025-06-23 16:59:14 +0300

    style: update font-family in globals.css to 'Cairo' and integrate Google Fonts in layout.tsx for improved typography consistency and performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="bg-[var(--background)] text-[var(--foreground)] min-h-screen antialiased">
         {children}
       </body>
