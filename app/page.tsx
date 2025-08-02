a343ae346b4f78547f857b1f05ff833aaf2c96d0
Isaladwy <Isaladwy@gmail.com>
2025-08-02 12:24:19 +0300

    feat: remove MenuSection component and integrate BuffetCategories and SpecialOffers directly in Home

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,26 @@ import Header from './components/Header';
 import Hero from './components/Hero/Hero';
 // import SpecialOccasion from './components/SpecialOccasion';
 // import ChefRecommend from './components/ChefRecommend/ChefRecommend';
-import MenuSection from './components/Menu/MenuSection';
 import CustomerReviews from './components/CustomerReviews/CustomerReviews';
 import Footer from './components/Footer';
 import ContactForm from './components/ContactForm/ContactForm';
+import BuffetCategories from './components/Menu/BuffetCategories';
+import SpecialOffers from './components/Menu/SpecialOffers';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Header />
       <Hero />
+      <div className="max-w-6xl mx-auto">
+
       {/* <SpecialOccasion /> */}
       {/* <ChefRecommend /> */}
-      <MenuSection />
+      <BuffetCategories />
+      <SpecialOffers />
       <CustomerReviews />
       <ContactForm />
+      </div>
       <Footer />
     </div>
   );
