eea1626d02c117b2b52c8f639f04daaf5f404b6c
Isaladwy <Isaladwy@gmail.com>
2025-08-03 14:52:09 +0300

    Enhance BuffetInfoCard layout with checklist style for menu items
    - Updated buffet menu items to display with checkmarks for better visual clarity.
    - Changed list structure to use <li> elements for improved semantic HTML.
    - Adjusted text color for menu items to enhance readability.

--- components/Menu/BuffetInfoCard.tsx
+++ components/Menu/BuffetInfoCard.tsx
@@ -5,12 +5,27 @@ export default function BuffetInfoCard() {
     <div className="flex relative items-center gap-3 justify-between bg-[#eaf5f2] rounded-2xl p-2 md:p-4 w-full mx-auto my-8  shadow-md">
       <div className="flex flex-col text-[#233a45] font-bold text-base md:text-lg leading-relaxed pr-1">
         <h2 className="font-bold text-2xl mb-5">قوائم البوفيه</h2>
-        <ul className="flex flex-col gap-2 drop-shadow-xs text-[var(--logo-green)]">
-          <span>أطباق رئيسية</span>
-          <span>مقبلات متنوعة</span>
-          <span>سرفيس خضار</span>
-          <span>مشروبات</span>
-          <span>حلويات</span>
+        <ul className="flex text-green-600 flex-col gap-2 drop-shadow-xs">
+          <li className="flex items-center gap-2">
+            <span className=" font-bold">✓</span>
+            أطباق رئيسية
+          </li>
+          <li className="flex items-center gap-2">
+            <span className=" font-bold">✓</span>
+            مقبلات متنوعة
+          </li>
+          <li className="flex items-center gap-2">
+            <span className=" font-bold">✓</span>
+            سرفيس خضار
+          </li>
+          <li className="flex items-center gap-2">
+            <span className=" font-bold">✓</span>
+            مشروبات
+          </li>
+          <li className="flex items-center gap-2">
+            <span className=" font-bold">✓</span>
+            حلويات
+          </li>
         </ul>
       </div>
       <div className="absolute w-[50%] sm:w-[65%] md:w-3/4 h-full left-0 rounded-2xl overflow-hidden z-0">
